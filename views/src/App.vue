2436f3c6a308d131e47dcdbd3688f5445964bdb7
RioH7 <humphreysrio1@aol.com>
2023-02-10 18:36:42 +0000

    Register component created

--- views/src/App.vue
+++ views/src/App.vue
@@ -1,9 +1,14 @@
 <template>
+    <RegisterComp />
 </template>
 
 <script>
+import RegisterComp from './components/RegisterComp.vue';
 
 export default {
   name: 'App',
+  components: {
+    RegisterComp
+  }
 }
 </script>
---
+++ views/src/components/RegisterComp.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    name: 'RegisterComp',
+    data() {
+        return {
+            name: '',
+            email: '',
+            pwd: '',
+            confirmPwd: ''
+        };
+    },
+    methods: {
+        submitForm() {
+            console.log(this.name, this.email, this.pwd, this.confirmPwd);
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>Sign Up</h1>
+        <form @submit.prevent="submitForm">
+            <div class="form-group">
+                <input type="text" id="name" v-model="name" placeholder="Name..." required />
+            </div>
+
+            <div class="form-group">
+                <input type="email" id="email" v-model="email" placeholder="Email..." required />
+            </div>
+
+            <div class="form-group">
+                <input type="password" id="pwd" placeholder="Password..." v-model="pwd" required />
+            </div>
+
+            <div class="form-group">
+                <input type="password" id="confirmPwd" placeholder="Confirm password..." v-model="confirmPwd" required />
+            </div>
+
+            <button type="submit" class="submit-button">Sign Up</button>
+        </form>
+    </div>
+</template>
+
+<style lang="sass" src="./styles/register.sass" scoped></style>
