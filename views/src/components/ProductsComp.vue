d8c47edd71a87f7eca1708eab1d4d4e15982fe19
RioH7 <humphreysrio1@aol.com>
2023-02-12 23:14:55 +0000

    product template page added

--- views/src/components/ProductsComp.vue
+++ views/src/components/ProductsComp.vue
@@ -1,21 +1,33 @@
 <template>
     <div class="container">
-        <form @submit.prevent="getProducts">
-            <button type="submit" class="submit-button">Sign Up</button>
-        </form>
+            <div class="col-3" v-for="product in products" :key="product._id">
+                <a href="#">
+                    <img :src="product.image" alt="product image">
+                </a>
+                <h4>{{ product.title }}</h4>
+                <p>{{ product.price }}</p>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'ProductsComp',
+    data() {
+        return {
+            products: [],
+        }
+    },
+    created() {
+        this.getProducts();
+    },
     methods: {
         async getProducts() {
             try {
                 const response = await fetch('http://localhost:3000/products', {mode: 'cors'});
                 if(response.ok) {
-                    const jsonResponse = await response.json();
-                    console.log(jsonResponse);
+                    const products = await response.json();
+                    this.products = products;
                 }
             } catch(error) {
                 console.log(error);
